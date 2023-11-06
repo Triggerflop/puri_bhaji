7960c38baf8a24eb9b041fa9cf6e2048ce465b2f
ParthDalvi164 <124716803+ParthDalvi164@users.noreply.github.com>
2023-11-07 01:17:30 +0530

    Add files via upload

---
+++ EXP11-NODESERVER/EXP7-CLASSFUN/Class1.js
@@ -0,0 +1,20 @@
+import React,{Component} from "react";
+
+class Class1 extends Component {
+    render() {
+        return(
+            <div>
+                <p>
+                    <h1> Class Component</h1>
+                </p>
+                <ul>
+                    <li>x</li>
+                    <li>y</li>
+                    <li>z</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Class1
---
+++ EXP11-NODESERVER/EXP7-CLASSFUN/Function.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Function() {
+    
+        return(
+            <div>
+                <p>
+                    <h1> Function Component</h1>
+                </p>
+                <ul>
+                    <li>x</li>
+                    <li>y</li>
+                    <li>z</li>
+                </ul>
+            </div>
+        )
+}
+
+export default Function
